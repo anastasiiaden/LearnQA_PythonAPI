f9337b69f8971adf54dc85b005f33f82032bf412
DESKTOP-V9304AA\anastasiia.denisova <anastasiia.denisova@idp.zyfra.com>
2022-03-19 00:19:44 +0300

    Задание 10. Тест с фразой

--- tests/test_phrase.py
+++ tests/test_phrase.py
@@ -2,4 +2,4 @@ class TestPhrase():
     def test_phrase(self):
         phrase = input("Set a phrase: ")
 
-        assert len(phrase) < 15, f"Phrase longer than 15 characters. It contains {len(phrase)} characters"
+        assert len(phrase) < 15, f"The phrase is longer than 15 characters. It contains {len(phrase)} characters"
